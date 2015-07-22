b362e477603bd947292938de96f788965d3e4edf
Joe Dytrych <j@dytry.ch>
2015-07-22 11:09:51 +0100

    Add windows build

--- build.sh
+++ build.sh
@@ -15,4 +15,6 @@ rm -rf ./.build/node_modules/electron-packager
 ./node_modules/.bin/electron-packager ./.build 'Oulipo' --platform=darwin --arch=x64 --version=0.28.1 --out=dist
 cp ./Info.plist ./dist/Oulipo.app/Info.plist
 
+./node_modules/.bin/electron-packager ./.build 'Oulipo' --platform=win32 --arch=x64 --version=0.28.1 --out=dist
+
 rm -rf .build
